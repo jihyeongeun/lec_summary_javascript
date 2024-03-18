a30757f045e3cad9462a6b1ba5b8a4204cc7f8ff
jihyeongeun <hyeongeun0924@gmail.com>
2024-03-18 12:44:05 +0900

    3.18 오전수업
    javascript let까지

--- 05_function/01_function-define/02_function-hoisting.js
+++ 05_function/01_function-define/02_function-hoisting.js
@@ -1,12 +1,20 @@
 /* 02. function hoisting(함수 호이스팅) */
 
+console.log(hello('이몽룡'));
 
 /* 함수 선언문(js 코드 상에서 먼저 해석됨, 반드시 이름이 있어야함(익명함수X)) */
-// function hello(name){
-    //     return `${name}님 반갑습니다.`;
-    // }
-    
-    var hello = function (name){
-        return `${name}님 반갑습니다.`;
-    }
-    console.log(hello('이몽룡'));
+function hello(name){
+    return `${name}님 반갑습니다.`;
+}
+
+/* 함수 호이스팅은 함수 선언문만 가능하고 함수 표현식은 불가능하다. */
+// var hello = function (name){
+//     return `${name}님 반갑습니다.`;
+// }
+
+/* 
+    함수 선언문은 런타임 이전 자바스크립트 엔진에 의해 먼저 해석된다.
+    따라서 함수 선언문 이전에 함수를 참조할 수 있으며 호출할 수도 있다.
+    함수 선언문이 코드의 선두로 끌어올려진 것처럼 동작하는 자바스크립트 
+    고유의 특징을 호이스팅이라고 한다.
+*/

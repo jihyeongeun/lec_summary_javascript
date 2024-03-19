3e2a131bf788c0d58ea2c1de27e8297da770f30e
jihyeongeun <hyeongeun0924@gmail.com>
2024-03-19 15:29:17 +0900

    3.19 lecture
    function-parameters finish

--- 02_javascript_es6/03_spread-syntax/02_array-and-object-copy/01_array-and-object-copy.js
+++ 02_javascript_es6/03_spread-syntax/02_array-and-object-copy/01_array-and-object-copy.js
@@ -12,8 +12,14 @@ console.log(arr === arrCopy);
 console.log(arr === arrCopy2);
 
 /* 2. 객체 복사 */
-let obj = {name: "홍길동", age: 20};
+let obj = {name: '홍길동', age: 20, addr: '서울시', hobby: ['축구', '농구']};
+
 let objCopy = {...obj};
+
+/* 추가적으로 나머지 연산자 개념으로도 활용도 가능하다. */
+/* let age = 30;
+let name = '강감찬'
+let objCopy = {...obj, name, age}; */
 
 console.log(obj);
 console.log(objCopy);
